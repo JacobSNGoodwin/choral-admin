3cc31eb00483c2a25d6d777f16983baecc7d81ab
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-11-04 20:56:58 -0800

    Cleanup more and test firebase config

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
-    <h1>{{ title }}</h1>
+  <section class="section">
+    <div class="container">
+      <h1>{{ title }}</h1>
+    </div>
+    <div class="container">
+      <app-login></app-login>
+    </div>
+  </section>
 </template>
 
 <script>
+import Login from '@/views/Login.vue';
+
 export default {
+  components: {
+    'app-login': Login,
+  },
   data() {
     return {
       title: 'In the app!',
