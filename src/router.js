d07076bdf01fcb5ee1632ef51b8ea42780091174
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-11-05 08:55:41 -0800

    Create AdminPanel view and accompanying route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import AdminPanel from './views/AdminPanel.vue';
 import Login from './views/Login.vue';
 
 Vue.use(Router);
@@ -9,9 +10,13 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'admin-panel',
+      component: AdminPanel,
+    },
+    {
+      path: '/login',
       name: 'login',
       component: Login,
-
     },
   ],
 });
