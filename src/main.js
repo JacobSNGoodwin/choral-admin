6c89ef22f67f96ebc170d6e9f06d0b845961c1ee
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-11-04 21:55:14 -0800

    Install and configure font awesome

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue';
 
+// Import font-awesome
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 // Require the main Sass manifest file
 import './assets/sass/main.scss';
+
+// Configure font awesome
+library.add(faEnvelope);
+dom.watch(); // Don't know where this is documented, but it gets it working!
 
 Vue.config.productionTip = false;
 
