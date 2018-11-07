5c89920605180d9c1bce83bc8c907c4aad039d51
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-11-06 16:50:55 -0800

    Add a list of admins to state and getter for this list

---
+++ src/store/modules/admin.js
@@ -0,0 +1,20 @@
+export default {
+  state: {
+    adminList: [
+      'John Doe',
+      'Jacob Goodwin',
+      'Jenna Gonzalez',
+    ],
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters: {
+    adminList(state) {
+      return state.adminList;
+    },
+  },
+};
--- src/views/Admins.vue
+++ src/views/Admins.vue
@@ -1,7 +1,20 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title has-text-centered">This is where we manage the dad gummed admins!</h1>
+      <h1 class="title has-text-centered has-text-weight-bold">Admins</h1>
+      <ul>
+        <li v-for="admin in adminList" :key="admin">{{ admin }}</li>
+      </ul>
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    adminList() {
+      return this.$store.getters.adminList;
+    },
+  },
+};
+</script>
