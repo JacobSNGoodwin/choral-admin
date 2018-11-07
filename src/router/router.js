69fe25047119db1e4ed8a3017079e47fce6aad89
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-11-06 21:12:30 -0800

    Add some more routes

--- src/router/router.js
+++ src/router/router.js
@@ -4,12 +4,16 @@ import Router from 'vue-router';
 import AdminPanel from '@/views/AdminPanel.vue';
 import Login from '@/views/Login.vue';
 import ManageAdmins from '@/views/ManageAdmins.vue';
+import ManagePerformances from '@/views/ManagePerformances.vue';
+import ManageAuditions from '@/views/ManageAuditions.vue';
+
 
 import adminAuthGuard from './admin-auth-guard';
 
 Vue.use(Router);
 
 const router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -25,6 +29,19 @@ const router = new Router({
       path: '/manage/admins',
       name: 'manageAdmins',
       component: ManageAdmins,
+      linkLabel: 'Manage Admins',
+    },
+    {
+      path: '/manage/performances',
+      name: 'managePerformances',
+      component: ManagePerformances,
+      linkLabel: 'Manage Performances',
+    },
+    {
+      path: '/manage/Auditions',
+      name: 'manageAuditions',
+      component: ManageAuditions,
+      linkLabel: 'Manage Auditions',
     },
   ],
 });
