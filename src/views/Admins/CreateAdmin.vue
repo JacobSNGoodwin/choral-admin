7a551cfb621f7b09c262d2ae95b952e1f3ad827e
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-11-29 12:47:30 -0800

    Start basic create admin form

--- src/views/Admins/CreateAdmin.vue
+++ src/views/Admins/CreateAdmin.vue
@@ -1,7 +1,55 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title has-text-centered has-text-weight-bold">Create New Admin</h1>
+      <div class="columns is-mobile is-centered">
+        <div class="column is-three-fifths-tablet is-two-fifths-desktop">
+          <h1 class="title has-text-centered has-text-weight-bold">Create New Admin</h1>
+          <form>
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="control has-icons-left">
+                <input
+                  :class="{'input': true, 'is-danger': errors.has('email') }"
+                  name="email"
+                  v-model="email"
+                  type="email"
+                  data-vv-delay="1000"
+                  v-validate="'email'"
+                  required
+                  placeholder="Email Address">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-envelope"></i>
+                </span>
+                <p
+                  v-if="errors.has('email')"
+                  class="help is-danger">{{ errors.first('email') }}</p>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Password</label>
+              <div class="control has-icons-left">
+                <input
+                  :class="{'input': true, 'is-danger': errors.has('password') }"
+                  name="password"
+                  v-model="password"
+                  type="password"
+                  v-validate="'min:6'"
+                  required
+                  placeholder="Password">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-key"></i>
+                </span>
+                <p
+                  v-if="errors.has('password')"
+                  class="help is-danger">{{ errors.first('password') }}</p>
+              </div>
+            </div>
+            <button class="button is-info"
+              :disabled="errors.any() || !email || !password"
+              type="submit">Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   </section>
 </template>
