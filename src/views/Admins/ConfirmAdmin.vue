7d5c67e94c138ef54a93a920a6e9214a46b7b8f1
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-12-05 12:23:41 -0800

    Add domain name environment variable
    This is necessary for redirect url in email verification sent from firebase

---
+++ src/views/Admins/ConfirmAdmin.vue
@@ -0,0 +1,43 @@
+<!-- <div class="field">
+              <label class="label">Password</label>
+              <div class="control has-icons-left">
+                <input
+                  ref="password"
+                  :class="{'input': true, 'is-danger': errors.has('password') }"
+                  name="password"
+                  v-model="password"
+                  type="password"
+                  data-vv-delay="1000"
+                  v-validate="'required|min:6'"
+                  placeholder="Password">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-key"></i>
+                </span>
+                <p
+                  v-if="errors.has('password')"
+                  class="help is-danger">{{ errors.first('password') }}</p>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Confirm Password</label>
+              <div class="control has-icons-left">
+                <input
+                  :class="{'input': true, 'is-danger': errors.has('confirmPassword') }"
+                  name="confirmPassword"
+                  v-model="confirmPassword"
+                  type="password"
+                  v-validate="'required|confirmed:password'"
+                  data-vv-as="password confirmation"
+                  placeholder="Confirm Password">
+                <span
+                  v-if="!errors.has('confirmPassword')
+                    && fields.confirmPassword
+                    && fields.confirmPassword.dirty"
+                  class="icon is-small is-left has-text-success">
+                  <i class="fas fa-check"></i>
+                </span>
+                <p
+                  v-if="errors.has('confirmPassword')"
+                  class="help is-danger">{{ errors.first('confirmPassword') }}</p>
+              </div>
+            </div> -->
