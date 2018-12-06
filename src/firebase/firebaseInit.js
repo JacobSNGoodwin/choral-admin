9ed2fc6fa7dc7b84fbc7fffc10d69630dece872e
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-12-06 10:25:57 -0800

    Fix init by adding secondary firebase app

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -5,6 +5,11 @@ import config from './firebaseConfig';
 
 firebase.initializeApp(config);
 
+// Secondary app used for merely creating a new user without signing out current user
+// Any database modification will need to be made by the default app
+// This user will not be able to modify the list of admins as it has not permissions
+const secondaryApp = firebase.initializeApp(config, 'Secondary');
+
 // Initialize Cloud Firestore through Firebase
 const db = firebase.firestore();
 
@@ -17,7 +22,7 @@ const performancesRef = db.collection('performances');
 const adminsRef = db.collection('admins');
 const authRef = firebase.auth();
 // second ref that allows us to create a new user without singning current user out
-const authRefForCreate = firebase.auth();
+const authRefForCreate = secondaryApp.auth();
 
 export {
   db,
