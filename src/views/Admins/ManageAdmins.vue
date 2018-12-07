454068e570f39bb15423f0fde982f93a8c340cba
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-12-06 18:26:39 -0800

    Add spinner icon while admin list is loading

--- src/views/Admins/ManageAdmins.vue
+++ src/views/Admins/ManageAdmins.vue
@@ -6,15 +6,23 @@
         <router-link
           class="button is-medium has-text-centered is-danger"
           to="/manage/admins/create">
-          <span class="icon is-medium">
+          <span class="icon">
             <i class="fas fa-plus-circle"></i>
           </span>
           <span>New Admin</span>
         </router-link>
       </div>
+      <div class="has-text-centered">
+        <span
+          class="icon is-large has-text-info has-text-centered"
+          v-if="loading">
+          <i class="fas fa-circle-notch fa-spin fa-3x"></i>
+        </span>
       <p
+        v-if="!loading"
         v-for="admin in adminList"
         :key="admin">{{admin}}</p>
+      </div>
     </div>
   </section>
 </template>
