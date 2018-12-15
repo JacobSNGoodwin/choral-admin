72f44446f42cc2ef437ee38fc8111bd76e672866
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-12-14 20:40:14 -0800

    Add delete admin function to delete admin credentials if removed from db

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.deleteAdmin = functions.firestore
+  .document('admins/{adminId}').onDelete((snap, context) => {
+    // if a admin document is deleted which is only allowed for admin users
+    // must also delete the users authentication
+    const uid = context.params.adminId;
+
+    return admin.auth().deleteUser(uid)
+      .catch(error => {
+        console.log("Error deleting user:", error);
+      });
+  });
+
