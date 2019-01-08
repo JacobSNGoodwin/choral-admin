3746e1f1d034c9bd164bd3df7231c9bb2e4de2da
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-01-07 20:12:16 -0800

    Scaffold our routes for createa and manage performances

---
+++ src/views/Performances/CreatePerformance.vue
@@ -0,0 +1,93 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns is-mobile is-centered">
+        <div class="column is-three-fifths-tablet is-two-fifths-desktop">
+          <h1 class="title has-text-centered has-text-weight-bold">Create New Performance</h1>
+          <form @submit.prevent="onCreateAdmin">
+            <div class="field">
+              <label class="label">Name</label>
+              <div class="control has-icons-left">
+                <input
+                  :class="{'input': true, 'is-danger': errors.has('name') }"
+                  name="name"
+                  v-model="name"
+                  data-vv-delay="500"
+                  v-validate="'required|min:2'"
+                  placeholder="Full Name">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-user"></i>
+                </span>
+                <p
+                  v-if="errors.has('name')"
+                  class="help is-danger">{{ errors.first('name') }}</p>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Role</label>
+              <div class="control has-icons-left">
+                <input
+                  :class="{'input': true, 'is-danger': errors.has('role') }"
+                  name="role"
+                  v-model="role"
+                  data-vv-delay="500"
+                  v-validate="'required|min:2'"
+                  placeholder="Role">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-user-tie"></i>
+                </span>
+                <p
+                  v-if="errors.has('role')"
+                  class="help is-danger">{{ errors.first('role') }}</p>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="control has-icons-left">
+                <input
+                  :class="{'input': true, 'is-danger': errors.has('email') }"
+                  name="email"
+                  v-model="email"
+                  type="email"
+                  data-vv-delay="500"
+                  v-validate="'required|email'"
+                  placeholder="Email Address">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-envelope"></i>
+                </span>
+                <p
+                  v-if="errors.has('email')"
+                  class="help is-danger">{{ errors.first('email') }}</p>
+              </div>
+            </div>
+            <button class="button is-info"
+              :disabled="errors.any() || hasInvalidInput"
+              type="submit">Create New Admin</button>
+          </form>
+          <p
+            v-if="errorMessage"
+            class="has-text-danger is-size-5 has-text-weight-semibold">{{errorMessage}}
+          </p>
+          <p
+            v-if="emailSent"
+            class="has-text-success is-size-5 has-text-weight-semibold">{{emailSent}}
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+  computed: {
+  },
+};
+</script>
+
---
+++ src/views/Performances/ManagePerformances.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title has-text-centered has-text-weight-bold">Manage Performances</h1>
+      <div class="has-text-centered">
+        <router-link
+          class="button is-medium has-text-centered is-danger"
+           :to="{name: 'createPerformance'}">
+          <span class="icon">
+            <i class="fas fa-plus-circle"></i>
+          </span>
+          <span>New Performance</span>
+        </router-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  computed: {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
